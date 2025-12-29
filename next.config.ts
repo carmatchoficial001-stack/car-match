58ce9c3e854be6079869e585160bbf5a0ad8d143
Ruben Carmatch <carmatchoficial001@gmail.com>
2025-12-28 17:00:59 -0700

    Fix next.config.ts Sentry arguments

--- next.config.ts
+++ next.config.ts
@@ -35,36 +35,30 @@ const nextConfig: NextConfig = {
 // Injected by Sentry
 import { withSentryConfig } from "@sentry/nextjs";
 
-export default withSentryConfig(nextConfig, {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
+// For all available options, see:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Suppresses source map uploading logs during build
-    silent: true,
+// Sentry Webpack Plugin Options
+silent: true,
     org: "carmatch",
-    project: "javascript-nextjs",
-}, {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+        project: "javascript-nextjs",
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+            // Sentry Next.js Options
+            // Upload a larger set of source maps for prettier stack traces (increases build time)
+            widenClientFileUpload: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
+                // Transpiles SDK to be compatible with IE11 (increases bundle size)
+                transpileClientSDK: true,
 
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
+                    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+                    tunnelRoute: "/monitoring",
 
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
+                        // Hides source maps from generated client bundles
+                        hideSourceMaps: true,
 
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
+                            // Automatically tree-shake Sentry logger statements to reduce bundle size
+                            disableLogger: true,
 
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
+                                // Enables automatic instrumentation of Vercel Cron Monitors.
+                                automaticVercelMonitors: true,
 });
